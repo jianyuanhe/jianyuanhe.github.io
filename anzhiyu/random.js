ca7725b30e871b686f14832ac98b47c8607bce81
jianyuanhe <2651135302@qq.com>
2025-10-29 17:44:22 +0800

    Site updated: 2025-10-29 17:44:20

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/10/26/hello-world/","2025/10/27/计算机网络结构基础/"];function toRandomPost(){
+var posts=["2025/10/26/hello-world/","2025/10/27/计算机网络结构基础/","2025/10/29/数据通信概念/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
