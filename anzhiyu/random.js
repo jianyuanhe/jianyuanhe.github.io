bf0f96d3fe5f94aed79471222488975547180036
jianyuanhe <2651135302@qq.com>
2025-10-29 17:31:44 +0800

    Site updated: 2025-10-29 17:31:41

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/10/26/hello-world/","2025/10/27/计算机网络的分类/"];function toRandomPost(){
+var posts=["2025/10/26/hello-world/","2025/10/27/计算机网络结构基础/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
