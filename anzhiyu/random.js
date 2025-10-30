f101c7437e20117de58fb4397cba2fba053aa550
jianyuanhe <2651135302@qq.com>
2025-10-30 16:58:34 +0800

    Site updated: 2025-10-30 16:58:31

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/10/26/hello-world/","2025/10/29/网规基础2数据通信概念/","2025/10/27/网规基础2计算机网络结构基础/","2025/10/30/网规基础3数据编码，调制技术与通信领域/"];function toRandomPost(){
+var posts=["2025/10/26/hello-world/","2025/10/30/网规基础3数据编码，调制技术与通信领域/","2025/10/29/网规基础2数据通信概念/","2025/10/27/网规基础2计算机网络结构基础/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
